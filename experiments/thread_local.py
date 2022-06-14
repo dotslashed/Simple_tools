fb715a26bf9bca95fc9c728c9e156d10588d7a67
Bhargab Kaushik <73475291+dotslashed@users.noreply.github.com>
2022-06-15 00:52:10 +0530

    Update thread_local.py

--- experiments/thread_local.py
+++ experiments/thread_local.py
@@ -1,11 +1,10 @@
-#Testing script for Thread's local object with requests for faster http requests. Backup_stuff
-
 import requests
 from requests import Session
 import threading
 import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
 from threading import Thread,local
+import urllib3
 
 with open('domains.txt', 'r') as f:
 	mass_urls = [line.strip() for line in f.readlines()]
@@ -19,8 +18,9 @@ def get_session():
 
 
 def gather_response(url):
+	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 	session = get_session()
-	with session.get(url) as response:
+	with session.get(url, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'}, allow_redirects = False, verify = False) as response:
 		try:
 
 			if 'Content-Length' not in response.headers or 'Content-Type' not in response.headers or 'Server' not in response.headers:
@@ -35,7 +35,7 @@ def gather_response(url):
 			print('Error!', url)
 
 def main(urls):
-	with ThreadPoolExecutor(max_workers=10) as executor:
+	with ThreadPoolExecutor(max_workers=2) as executor:
 		executor.map(gather_response,mass_urls)
 
 main(mass_urls)
